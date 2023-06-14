e027688a959458c3727edb8b6cc5d5d529496f34
Caroline Cerussi <caroceru@gmail.com>
2023-06-14 14:23:52 -0700

    work on adding new header styling in react component

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,44 @@ import React from 'react';
 import './index.css';
 
 function Header() {
-    return (
-        <div className="header">
-            <h1>Caroline Cerussi</h1>
+    const caroline = {
+		color: "black",
+		fontSize: '100pt',
+		fontFamily: "KiTestRings OutliNe",
+		backgroundColor: 'transparent',
+        width: '50%',
+          
+        }
+          
+    const aboutHead = {
+		color: "black",
+		fontSize: '60pt',
+		fontFamily: "KiTestRings OutliNe",
+		backgroundColor: 'transparent',
+        width: '50%',
+	}
+    const carolinePhoto = {
+		backgroundColor: 'red',
+        width: '50%',
 
+	}
+
+
+    return (
+        <React.Fragment>
+
+        <div className="carolineHead" style={caroline}>
+            <h1>Caroline Cerussi</h1>
         </div>
+            <div className="aboutHead" style={aboutHead}>
+            <h2>Front End Designer</h2>
+        </div>
+        <div className="carolinePhoto" style={carolinePhoto}>
+           {/* <img src={face} alt='face' /> */}
+        </div>
+
+        </React.Fragment>
     )
+
 }
 export default Header;
