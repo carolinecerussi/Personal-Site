5e30efbdcf66efeb1e88ee4d1d810fd752f5db9a
Caroline Cerussi <caroceru@gmail.com>
2023-09-27 18:24:17 -0700

    add finn pop up

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -6,7 +6,7 @@ import Popup from 'reactjs-popup';
 import photo from "../img/chair_.png";
 import photo2 from "../img/flash.png";
 import chair from "../img/heart.png";
-
+import finn from "../img/Finn.png";
 function DraggableThing() {
     return (
         <React.Fragment>
@@ -21,7 +21,7 @@ function DraggableThing() {
 
         <div className='dragBox'>
         <div className="hidden"><p><i class="fa fa-arrows-alt"></i></p>
-          <Popup  trigger={<img src={photo} width="100%"/>} position="left"><p className="pop"><img src={chair} width='20%' /> hi</p>
+          <Popup  trigger={<img id="chair" src={photo} width="100%"/>} position="left"><p className="pop"><img  src={chair} width='20%' /> hi</p>
           </Popup>
           </div>
         </div>
@@ -39,13 +39,30 @@ function DraggableThing() {
         <div className='dragBox'>
         <div className="hidden"><p><i class="fa fa-arrows-alt"></i>
 </p>
-          <Popup trigger={<img src={photo2} width="100%"/>} position="left"><p className="pop"> hey</p>
+          <Popup trigger={<img id="flash" src={photo2} width="100%"/>} position="left"><p className="pop"> hey</p>
           </Popup>
           </div>
         </div>
       </Draggable>
 
      
+    
+      <Draggable
+        handle=".dragBox"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[30, 30,]}
+        scale={1}
+>
+        <div className='dragBox'>
+        <div className="hidden"><p><i class="fa fa-arrows-alt"></i>
+</p>
+          <Popup trigger={<img id="flash" src={finn} width="70%"/>} position="left"><p className="pop"> hey</p>
+          </Popup>
+          </div>
+        </div>
+      </Draggable>
+
       </div>
 </React.Fragment>
     )
